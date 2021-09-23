cb649b94cdb9cf90235f2f3111e511ebd707c087
Josue Leon Sarkis <jolesa97@gmail.com>
2021-09-23 13:01:05 -0600

    Initial ui code

---
+++ src/components/orders/Orders.js
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+function Orders() {
+  const getOrders = () => axios.get("http://localhost:8080/api/orders");
+
+  const [orders, setOrders] = useState([]);
+  useEffect(() => {
+    getOrders().then((response) => {
+      setOrders(response.data);
+    });
+  }, []);
+
+  let orderItems = <h1>Order now!</h1>;
+
+  if (orders.length > 0) {
+    orderItems = orders.map((order) => (
+      <div key={order.id}>
+        <h2>{order.id}</h2>
+        <div>
+          <h3>Pizza</h3>
+          <h4>Name: {order.pizza.name}</h4>
+          {order.pizza.toppings.map((topping) => (
+            <div key={topping.id}>{topping.name}</div>
+          ))}
+          <h4>Crust: {order.crust.name}</h4>
+          <h4>Size: {order.size.size}</h4>
+        </div>
+      </div>
+    ));
+  }
+
+  return <div>{orderItems}</div>;
+}
+
+export default Orders;
